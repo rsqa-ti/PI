3bbdf77ed8f1031d39e1ecf486b79e6a6cb5ca8d
Ricardo Assis <ricardo.quina@hotmail.com>
2020-05-26 14:56:14 -0300

    Teste

---
+++ TelasPI/TelasPI/.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TelasPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btAgendamento = new System.Windows.Forms.Button();
+            this.btCadastro = new System.Windows.Forms.Button();
+            this.btEstoque = new System.Windows.Forms.Button();
+            this.btOrcamento = new System.Windows.Forms.Button();
+            this.btUsuario = new System.Windows.Forms.Button();
+            this.btSairTelaInicio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btAgendamento
+            // 
+            this.btAgendamento.Location = new System.Drawing.Point(265, 137);
+            this.btAgendamento.Name = "btAgendamento";
+            this.btAgendamento.Size = new System.Drawing.Size(100, 30);
+            this.btAgendamento.TabIndex = 0;
+            this.btAgendamento.Text = "Agendamento";
+            this.btAgendamento.UseVisualStyleBackColor = true;
+            // 
+            // btCadastro
+            // 
+            this.btCadastro.Location = new System.Drawing.Point(397, 137);
+            this.btCadastro.Name = "btCadastro";
+            this.btCadastro.Size = new System.Drawing.Size(100, 30);
+            this.btCadastro.TabIndex = 1;
+            this.btCadastro.Text = "Cadastro";
+            this.btCadastro.UseVisualStyleBackColor = true;
+            // 
+            // btEstoque
+            // 
+            this.btEstoque.Location = new System.Drawing.Point(397, 209);
+            this.btEstoque.Name = "btEstoque";
+            this.btEstoque.Size = new System.Drawing.Size(100, 30);
+            this.btEstoque.TabIndex = 3;
+            this.btEstoque.Text = "Estoque";
+            this.btEstoque.UseVisualStyleBackColor = true;
+            // 
+            // btOrcamento
+            // 
+            this.btOrcamento.Location = new System.Drawing.Point(265, 209);
+            this.btOrcamento.Name = "btOrcamento";
+            this.btOrcamento.Size = new System.Drawing.Size(100, 30);
+            this.btOrcamento.TabIndex = 2;
+            this.btOrcamento.Text = "Orçamento";
+            this.btOrcamento.UseVisualStyleBackColor = true;
+            // 
+            // btUsuario
+            // 
+            this.btUsuario.Location = new System.Drawing.Point(56, 388);
+            this.btUsuario.Name = "btUsuario";
+            this.btUsuario.Size = new System.Drawing.Size(100, 30);
+            this.btUsuario.TabIndex = 4;
+            this.btUsuario.Text = "Usuário";
+            this.btUsuario.UseVisualStyleBackColor = true;
+            // 
+            // btSairTelaInicio
+            // 
+            this.btSairTelaInicio.Location = new System.Drawing.Point(625, 388);
+            this.btSairTelaInicio.Name = "btSairTelaInicio";
+            this.btSairTelaInicio.Size = new System.Drawing.Size(100, 30);
+            this.btSairTelaInicio.TabIndex = 5;
+            this.btSairTelaInicio.Text = "Sair";
+            this.btSairTelaInicio.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btSairTelaInicio);
+            this.Controls.Add(this.btUsuario);
+            this.Controls.Add(this.btEstoque);
+            this.Controls.Add(this.btOrcamento);
+            this.Controls.Add(this.btCadastro);
+            this.Controls.Add(this.btAgendamento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAgendamento;
+        private System.Windows.Forms.Button btCadastro;
+        private System.Windows.Forms.Button btEstoque;
+        private System.Windows.Forms.Button btOrcamento;
+        private System.Windows.Forms.Button btUsuario;
+        private System.Windows.Forms.Button btSairTelaInicio;
+    }
+}
---
+++ TelasPI/TelasPI/Login.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace TelasPI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(355, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(355, 151);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(345, 230);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(75, 23);
+            this.btLogin.TabIndex = 4;
+            this.btLogin.Text = "Entrar";
+            this.btLogin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btLogin;
+    }
+}
+
